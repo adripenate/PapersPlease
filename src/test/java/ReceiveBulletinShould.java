a64287dd2389a1ff598f34788e17c749722dd2a0
adriana <adrianapenate98@gmail.com>
2019-10-22 18:29:31 +0100

    Red test - ReceiveBulletinShould.assign_bulletin

--- src/test/java/ReceiveBulletinShould.java
+++ src/test/java/ReceiveBulletinShould.java
@@ -1,11 +1,22 @@
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
 
 public class ReceiveBulletinShould {
     @Test
-    public void assing_bulletin() {
+    public void assign_bulletin() {
         Bulletin bulletin = new Bulletin(new String[]{"Arstotzka"}, new Class[]{ID_Card.class},
                 null, new String[]{"Pepe Viyuela"});
 
+        OfficerRepository officerRepository = mock(OfficerRepository.class);
+        when(officerRepository.recover()).thenReturn(new Officer());
+        ArgumentCaptor<Officer> argumentCaptor = ArgumentCaptor.forClass(Officer.class);
+        verify(officerRepository).save(argumentCaptor.capture());
+
+        new ReceiveBulletin().execute(bulletin, officerRepository, 1);
+
+        assertThat(argumentCaptor.getValue().bulletin).isEqualToComparingFieldByField(bulletin);
     }
 
     private class Bulletin {
@@ -23,5 +34,26 @@ public class ReceiveBulletinShould {
     }
 
     private class ID_Card {
+    }
+
+    private class Officer {
+        private Bulletin bulletin;
+
+        public void receiveBulletin(Bulletin bulletin) {
+            this.bulletin = bulletin;
+        }
+
+    }
+
+    private interface OfficerRepository {
+        Officer recover();
+
+        void save(Officer officer);
+    }
+
+    private class ReceiveBulletin {
+        public void execute(Bulletin bulletin, OfficerRepository officerRepository, int gameID) {
+
+        }
     }
 }
