791d6ea96701bdaf2bb6e9a572d1c27e51c34940
adriana <adrianapenate98@gmail.com>
2019-11-07 20:08:22 +0000

    Red test - let_entrant_pass_id_IDCard_is_present

--- src/main/java/papersplease/ID_Card.java
+++ src/main/java/papersplease/ID_Card.java
@@ -1,4 +1,17 @@
 package papersplease;
 
 public class ID_Card {
+    private final String district;
+    private final String name;
+    private final String dateOfBirth;
+    private final int height;
+    private final int weight;
+
+    public ID_Card(String district, String name, String dateOfBirth, int height, int weight) {
+        this.district = district;
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.height = height;
+        this.weight = weight;
+    }
 }
---
+++ src/test/java/InspectShould.java
@@ -0,0 +1,41 @@
+import org.junit.Test;
+import papersplease.Bulletin;
+import papersplease.ID_Card;
+import papersplease.Officer;
+import papersplease.repository.OfficerRepository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+public class InspectShould {
+    @Test
+    public void let_entrant_pass_if_IDCard_is_present() {
+        Bulletin bulletin = new Bulletin(new String[]{"Arstotzka"}, new Class[]{ID_Card.class},
+                null, new String[]{"Pepe Viyuela"});
+        int gameID = 1;
+        OfficerRepository officerRepository = mock(OfficerRepository.class);
+        Officer officer = new Officer();
+        officer.receiveBulletin(bulletin);
+        when(officerRepository.recover(gameID)).thenReturn(officer);
+        Entrant entrant = new Entrant();
+        entrant.giveIDCard(new ID_Card("District", "Name", "01/01/1999", 1, 1));
+
+        String response = new Inspect().execute(officerRepository, gameID, entrant);
+
+        assertThat(response).isEqualTo("Glory to Arstotzka.");
+    }
+
+    private class Inspect {
+        public String execute(OfficerRepository officerRepository, int gameID, Entrant entrant) {
+            return null;
+        }
+    }
+
+    private class Entrant {
+        private ID_Card id_card;
+
+        public void giveIDCard(ID_Card id_card) {
+            this.id_card = id_card;
+        }
+    }
+}
